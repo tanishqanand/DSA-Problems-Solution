703303d32d2445e10c192b48b014e05b2fe75fa5
Tanishq Anand <tanishq0087@gmail.com>
2022-12-12 12:14:52 +0530

    Day 2 Question

---
+++ LargestElementInArray.java
@@ -0,0 +1,35 @@
+/*
+Question:
+Given an array A[] of size n. The task is to find the largest element in it.
+ */
+
+import java.util.Scanner;
+
+public class LargestElementInArray {
+    public static int largestElement(int[] arr) {
+        int maxi = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++) {
+            if(maxi < arr[i])
+                maxi = arr[i];
+        }
+        return maxi;
+    }
+    public static int[] takeInput() {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = takeInput();
+        int largest = largestElement(arr);
+
+        System.out.println("Largest Element is: " + largest);
+    }
+}
