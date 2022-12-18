bb29f3b9e25ee639f465ddb4a97969342768c84d
Tanishq Anand <tanishq0087@gmail.com>
2022-12-18 23:06:51 +0530

    Day 5

---
+++ BubbleSort.java
@@ -0,0 +1,41 @@
+/*
+Question:
+Given an Integer array arr. Sort the array using bubble sort algorithm.
+ */
+
+import java.util.Scanner;
+
+public class BubbleSort {
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    private static void bubbleSort(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1])
+                    swap(arr, j, j + 1);
+            }
+        }
+    }
+    private static void printArray(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    private static int[] takeInput() {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+        for(int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        int[] arr = takeInput();
+        bubbleSort(arr);
+        printArray(arr);
+    }
+}
