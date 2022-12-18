770f2cbe0dc9eac3228169d87ffb46eb6720c863
Tanishq Anand <tanishq0087@gmail.com>
2022-12-18 23:11:25 +0530

    Day 5

--- BubbleSort.java
+++ BubbleSort.java
@@ -12,8 +12,8 @@ public class BubbleSort {
         arr[j] = temp;
     }
     private static void bubbleSort(int[] arr) {
-        for(int i = 0; i < arr.length; i++) {
-            for(int j = 0; j < arr.length - 1; j++) {
+        for(int i = 0; i < arr.length - 1; i++) {
+            for(int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1])
                     swap(arr, j, j + 1);
             }
